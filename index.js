c8fadda6aff04b108a0e318b554e54ab0285c6d6
Alex Rodriguez <alexrod102797@gmail.com>
2020-09-13 23:34:52 -0400

    couldnt get settimeout in forloop. will fix when i can.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const burger = document.getElementById('burger');
-const sideNavBar = document.getElementById('sideNavBar')
+const burger = document.getElementById('burger'); //menu icon on navbar
+const sideNavBar = document.getElementById('sideNavBar'); 
+const roleText = document.getElementById('dynamic-role'); //changing role following "i am a..."
 let sideNavBarOpen = false;
+const roles = ['developer', 'freelancer', 'problem solver'];
+dynamicElementText(roleText, roles);
 
-burger.addEventListener('click', function(sideNavBarOpen, sideNavBar){
-    sideNavBarOpen = !sideNavBarOpen;
+burger.addEventListener('click', function(){
+    sideNavBarOpen = !sideNavBarOpen; //
     extendOrCollapseWidth(sideNavBarOpen, sideNavBar);
 });
+
--- utility.js
+++ utility.js
@@ -1,9 +1,20 @@
 function extendOrCollapseWidth(isToggled, sideNavBar){
-    console.log('hello?')
-    console.log(isToggled);
     if(isToggled){
-        sideNavBar.style.width = '250px'
+        sideNavBar.style.width = '6vh';
+        sideNavBar.style.padding = '1%';
     }else{
-        sideNavBar.style.width = '0px'
+        sideNavBar.style.width = '0';
+        sideNavBar.style.padding = '0';
     }
+}
+
+function dynamicElementText(domElement, arr){
+    let innerText = '';
+    for(let i = 0; i < arr.length; i++){
+        for(let n = 0; n < arr[i].length; n++){
+            //wait 1 second, then execute something
+            
+        }
+    }
+  domElement.innerText = innerText;
 }
