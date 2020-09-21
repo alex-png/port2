a89bd514fecee76235ec8105773ac071409c482e
Alex Rodriguez <alexrod102797@gmail.com>
2020-09-21 14:56:47 -0400

    sidebar color changing

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const burger = document.getElementById('burger'); //menu icon on navbar
-const sideNavBar = document.getElementById('sideNavBar'); 
-const roleText = document.getElementById('dynamic-role'); //changing role following "i am a..."
+const burger = document.getElementById("burger"); //menu icon on navbar
+const sideNavBar = document.getElementById("sideNavBar");
+const roleText = document.getElementById("dynamic-role"); //changing role following "i am a..."
 let sideNavBarOpen = false;
-const roles = ['developer', 'freelancer', 'problem solver'];
+let clickedSection = "";
+const roles = ["developer", "freelancer", "problem solver"];
 
 animatedText(roleText, roles);
 
-burger.addEventListener('click', function(){
-    sideNavBarOpen = !sideNavBarOpen; //
-    extendOrCollapseWidth(sideNavBarOpen, sideNavBar);
+burger.addEventListener("click", function () {
+  sideNavBarOpen = !sideNavBarOpen; //
+  extendOrCollapseWidth(sideNavBarOpen, sideNavBar);
 });
 
-sideNavBar.addEventListener('click', function(){
-    let clickedSection = event.target;
-    
-    if(clickedSection.id === "home"){
-        
-    }else if(clickedSection.id === "about"){
-
-    }else if(clickedSection.id === "portfolio"){
-        
-    }else if(clickedSection.id === "contact"){
-
+sideNavBar.addEventListener("click", function () {
+  if (event.target.className === "sideNavBar-section") {
+    if (clickedSection) {
+      clickedSection.style.background = "";
+      clickedSection = event.target;
+      clickedSection.style.background = "steelblue";
+    } else {
+      clickedSection = event.target;
+      clickedSection.style.background = "steelblue";
     }
+  }
 });
 //////am i being committed?
